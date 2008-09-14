acb10ed62829ca763d0921187156233c8caacfac
Juliano F. Ravasi <dev@juliano.info>
2008-09-14 01:18:01 +0000

    Portuguese translation update.

--- Wikilog.i18n.php
+++ Wikilog.i18n.php
@@ -14,13 +14,13 @@ $messages['en'] = array(
 	# Special:Wikilog
 	'wikilog'					=> 'Wikilogs',	# Page title
 	'wikilog-specialwikilog'	=> 'Wikilog',	# Special page name
-	'wikilog-summary'			=> '',			# Special page summary
 
 	# Extension description
 	'wikilog-desc'				=> 'Adds blogging features, creating a wiki-blog hybrid.',
 
 	# Generic strings
 	'wikilog-published'			=> 'Published',
+	'wikilog-draft'				=> 'Draft',
 	'wikilog-authors'			=> 'Authors',
 	'wikilog-wikilog'			=> 'Wikilog',
 	'wikilog-title'				=> 'Title',
@@ -35,10 +35,6 @@ $messages['en'] = array(
 	'wikilog-pager-next'		=> 'next →',
 	'wikilog-pager-first'		=> '⇇ first',
 	'wikilog-pager-last'		=> 'last ⇉',
-	'wikilog-navigation-bar'	=>
-		'<div class="wl-navbar visualClear"><div style="float:left">$1 • $2</div>'.
-		'<div style="float:right">$3 • $4</div>'.
-		'<div style="text-align:center">$5</div></div>',
 	'wikilog-pager-empty'		=>
 		'<div class="wl-empty">(no items)</div>',
 
@@ -92,6 +88,13 @@ $messages['en'] = array(
 	'wikilog-show-all'			=> 'All posts',
 	'wikilog-show-published'	=> 'Published',
 	'wikilog-show-drafts'		=> 'Drafts',
+
+	# Untranslatable strings
+	'wikilog-summary'			=> '',			# Special page summary
+	'wikilog-navigation-bar'	=>
+		'<div class="wl-navbar visualClear"><div style="float:left">$1 • $2</div>'.
+		'<div style="float:right">$3 • $4</div>'.
+		'<div style="text-align:center">$5</div></div>',
 );
 
 /** Portuguese (Português)
@@ -105,19 +108,74 @@ $messages['pt'] = array(
 	# Extension description
 	'wikilog-desc'				=> 'Adiciona recursos de blog, criando um híbrido wiki-blog.',
 
+	# Generic strings
+	'wikilog-published'			=> 'Publicado',
+	'wikilog-draft'				=> 'Rascunho',
+	'wikilog-authors'			=> 'Autores',
+	'wikilog-wikilog'			=> 'Wikilog',
+	'wikilog-title'				=> 'Título',
+	'wikilog-actions'			=> 'Ações',
+
 	# Pager strings
 	'wikilog-pager-newer-n'		=> '← $1 próximos',
 	'wikilog-pager-older-n'		=> '$1 anteriores →',
 	'wikilog-pager-newest'		=> '⇇ mais recentes',
 	'wikilog-pager-oldest'		=> 'mais antigos ⇉',
-	'wikilog-navigation-bar'	=>
-		'<div class="wl-navbar"><div style="float:left">$1 • $2</div>'.
-		'<div style="float:right">$3 • $4</div>'.
-		'<div style="text-align:center">$5</div></div>',
+	'wikilog-pager-prev'		=> '← anterior',
+	'wikilog-pager-next'		=> 'próxima →',
+	'wikilog-pager-first'		=> '⇇ primeira',
+	'wikilog-pager-last'		=> 'última ⇉',
 	'wikilog-pager-empty'		=>
 		'<div class="wl-empty">(não há itens)</div>',
 
 	# Comments page link text
 	'wikilog-no-comments'		=> "não há comentários",
 	'wikilog-has-comments'		=> "{{PLURAL:\$1|um comentário|\$1 comentários}}",
+
+	'wikilog-author-signature'	=> "[[{{ns:User}}:\$1|\$1]] ([[{{ns:User_talk}}:\$1|talk]])",
+	'wikilog-item-brief-header'	=> ": ''<small>por \$5, em [[\$1|\$2]], \$6.</small>''",
+	'wikilog-item-brief-footer'	=> "",
+	'wikilog-item-more'			=> "\n[[\$3|&rarr; continuar lendo...]]\n",
+	'wikilog-item-sub'			=> "",
+	'wikilog-item-header'		=> "",
+	'wikilog-item-footer'		=> ": ''&mdash; \$5 &#8226; \$6 &#8226; \$7''\n",
+	'wikilog-comments-header'	=> "",
+	'wikilog-comments-footer'	=> "",
+
+	# Atom and RSS feeds
+	'wikilog-feed-title'		=> "{{SITENAME}} - $1 [$2]", # $1 = title, $2 = content language
+	'wikilog-feed-description'	=> "Leia as postagens mais recentes neste feed.",
+
+	# Warning and error messages
+	'wikilog-invalid-author'	=> "Wikilog: Autor inválido: \$1.",
+	'wikilog-invalid-date'		=> "Wikilog: Data inválida: \$1.",
+	'wikilog-invalid-tag'		=> "Wikilog: Rótulo inválido: \$1.",
+	'wikilog-draft-title-mark'	=> "(rascunho)",
+	'wikilog-out-of-context'	=>
+			"Aviso: Rótulos wikilog estão sendo utilizados fora de contexto. " .
+			"Eles devem ser usados apenas em artigos no espaço de nomes do Wikilog.",
+	'wikilog-too-many-authors'	=>
+			"Aviso: Autores demais listados nesta postagem wikilog.",
+	'wikilog-too-many-tags'	=>
+			"Aviso: Rótulos demais listados nesta postagem wikilog.",
+	'wikilog-reading-draft'		=>
+			"<div class='mw-warning'>\n".
+			"<center>'''Este artigo é um rascunho.'''</center>\n\n".
+			"Esta postagem wikilog é um rascunho, e ainda não foi publicado. ".
+			"Para publicar uma postagem wikilog, você deve adicionar a tag ".
+			"<code><nowiki>{{wl-publish:...}}</nowiki></code> ao texto do ".
+			"artigo, fornecendo opcionalmente a data de publicação e um ou ".
+			"mais autores.\n</div>\n",
+
+	# Forms
+	'wikilog-form-legend'		=> 'Procurar por postagens wikilog',
+	'wikilog-form-wikilog'		=> 'Wikilog:',
+	'wikilog-form-category'		=> 'Categoria:',
+	'wikilog-form-author'		=> 'Autor:',
+	'wikilog-form-tag'			=> 'Rótulo:',
+	'wikilog-form-date'			=> 'Data:',
+	'wikilog-form-status'		=> 'Estado:',
+	'wikilog-show-all'			=> 'Todas as postagens',
+	'wikilog-show-published'	=> 'Publicados',
+	'wikilog-show-drafts'		=> 'Rascunhos',
 );
