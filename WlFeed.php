7ae4bd1048e76f94e33b3e3f08399a836f161645
Juliano F. Ravasi <dev@juliano.info>
2009-06-11 02:33:08 +0000

    Bump to version 0.7.0.1svn.

--- WlFeed.php
+++ WlFeed.php
@@ -34,7 +34,7 @@ if ( !defined( 'MEDIAWIKI' ) )
  */
 $wgExtensionCredits['other'][] = array(
 	'name'				=> 'WlFeed',
-	'version'			=> '0.7.0',
+	'version'			=> '0.7.0.1svn',
 	'author'			=> 'Juliano F. Ravasi',
 	'description'		=> 'Enhanced feed generation classes.',
 	'descriptionmsg'	=> 'wlfeed-desc',
