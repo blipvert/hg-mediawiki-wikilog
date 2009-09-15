ebad8bfa049a0d5cba9f4188b5f059d195eee7dc
Juliano F. Ravasi <dev@juliano.info>
2009-09-15 17:46:56 -0300

    Wikilog version 0.8.1.

--- WlFeed.php
+++ WlFeed.php
@@ -34,7 +34,7 @@ if ( !defined( 'MEDIAWIKI' ) )
  */
 $wgExtensionCredits['other'][] = array(
 	'name'				=> 'WlFeed',
-	'version'			=> '0.8.0',
+	'version'			=> '0.8.1',
 	'author'			=> 'Juliano F. Ravasi',
 	'description'		=> 'Enhanced feed generation classes.',
 	'descriptionmsg'	=> 'wlfeed-desc',
